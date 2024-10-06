e5ca1b6501c2d100344eeefd934c0baaab280f6e
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2024-10-06 20:48:55 +0545

    fix(Post request)and create_query

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Province, District, Ward, Place
 from .serializers import ProvinceSerializer, DistrictSerializer, WardSerializer, PlaceSerializer
+from rest_framework.permissions import AllowAny
 
 class ProvinceViewSet(viewsets.ModelViewSet):
     queryset = Province.objects.all()
@@ -12,14 +15,27 @@ class DistrictViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return District.objects.filter(province_id=self.kwargs['province_pk'])
 
+    def perform_create(self, serializer):
+        province = Province.objects.get(pk=self.kwargs['province_pk'])
+        serializer.save(province=province)
+
 class WardViewSet(viewsets.ModelViewSet):
     serializer_class = WardSerializer
 
     def get_queryset(self):
         return Ward.objects.filter(district_id=self.kwargs['district_pk'])
 
+    def perform_create(self, serializer):
+        district = District.objects.get(pk=self.kwargs['district_pk'])
+        serializer.save(district=district)
+        permission_classes = [AllowAny]
+
 class PlaceViewSet(viewsets.ModelViewSet):
     serializer_class = PlaceSerializer
 
     def get_queryset(self):
         return Place.objects.filter(ward_id=self.kwargs['ward_pk'])
+
+    def perform_create(self, serializer):
+        ward = Ward.objects.get(pk=self.kwargs['ward_pk'])
+        serializer.save(ward=ward)
